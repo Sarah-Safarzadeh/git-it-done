82f085b22be31eda5fb3cf210c3dbc708d439057
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-05 15:56:13 -0500

    created displayRepo function

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,9 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+// FUNCTIONS
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
@@ -5,9 +11,32 @@ var getUserRepos = function(user) {
     // make a request to the url
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
-        console.log(data);
+       displayRepos(data, user);
       });
     });
   };
-  
-getUserRepos();
+
+var formSubmitHandler = function(event) {
+event.preventDefault();
+// Get value from input element
+var username = nameInputEl.nodeValue.trim();
+
+if (username) {
+  getUserRepos(username);
+  nameInputEl.value = "";
+} else {
+  alert("Please enter a GitHub username");
+}
+console.log(event);
+};
+
+var displayRepos = function(repos, searchTerm) {
+  console.log(repos)
+  console.log(searchTerm);
+  // Clear Old Content
+  repoContainerEl.textContent = "";
+  repoSearchTerm.textContent = searchTerm;
+};
+
+// EVENT LISTENERS
+userFormEl.addEventListener("submit", formSubmitHandler);
