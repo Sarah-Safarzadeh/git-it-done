85a340f3d7828ecd6012e85c3ea306c26d2fc72e
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-05 16:31:26 -0500

    modified fetch

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -8,16 +8,22 @@ var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
   
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
+    // Make a Request to the URL
+    fetch(apiUrl)
+    .then(function(response) {
+      // Request Was Successful
       if (response.ok) {
         response.json().then(function(data) {
           displayRepos(data, user);
-      });
-    } else {
-      alert("Error: " + response.statusText);
-    }
-  });
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      // Notice this `.catch()` getting chained onto the end of the `.then()` method
+      alert("Unable to connect to GitHub");
+    });
 };
 
 var formSubmitHandler = function(event) {
