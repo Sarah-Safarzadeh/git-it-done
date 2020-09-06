8992be410fd192217d13865636e420450c15b63e
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-06 15:29:07 -0500

    updated fetch request and checked response

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,7 +1,16 @@
 var getRepoIssues = function(repo) {
 console.log(repo);
 var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-fetch(apiUrl);
+fetch(apiUrl).then(function(response) {
+    // Request Was Successful
+    if (response.ok) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    } else {
+        alert("there was a problem with your request!");
+    }
+});
 };
 
 getRepoIssues("facebook/react");
