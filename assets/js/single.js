030317b8d893db55b3d0764d57de8a0829bf9ce3
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-06 16:51:17 -0500

    added displauWarning function

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,7 @@
 var issueContainerEl = document.querySelector("#issues-container");
 
+var limitWarningEl = document.querySelector("#limit-warning");
+
 var getRepoIssues = function (repo) {
     console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -50,10 +52,31 @@ var getRepoIssues = function (repo) {
             alert("there was a problem with your request!");
         }
     });
+    if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+      
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+            console.log("repo has more than 30 issues");
+          }
+        });
+      }
 };
 var displayRepos = function (issues) {
 
-}
+};
 
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // Append to Warning Container
+    limitWarningEl.appendChild(linkEl);
+};
 
 getRepoIssues("facebook/react");
