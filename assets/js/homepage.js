6fd95069a143099d4c818efde4168a7234b079f9
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-06 17:00:16 -0500

    changed dynamic div element in displayRepos function to create a element

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -55,8 +55,9 @@ var displayRepos = function(repos, searchTerm) {
   for (var i = 0; i < repos.length; i++) {
     var repoName = repos[i].owner.login + "/" + repos[i].name;
     // Create A Container For Each Repo
-    var repoEl = document.createElement("div");
+    var repoEl = document.createElement("a");
     repoEl.classList = "list-item flex-row justify-space-between align-center";
+    repoEl.setAttribute("href", "./single-repo.html");
     // Create a Span Element to Hold Repository Name
     var titleEl = document.createElement("span");
     titleEl.textContent = repoName;
