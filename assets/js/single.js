59f07ee76545bc9af1db64abc3b41f9515302f0e
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-06 15:58:32 -0500

    added for loop in displayIssues function

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,16 +1,50 @@
-var getRepoIssues = function(repo) {
-console.log(repo);
-var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
-fetch(apiUrl).then(function(response) {
-    // Request Was Successful
-    if (response.ok) {
-        response.json().then(function(data) {
-            console.log(data);
-        });
-    } else {
-        alert("there was a problem with your request!");
-    }
-});
+var getRepoIssues = function (repo) {
+    console.log(repo);
+    var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    fetch(apiUrl).then(function (response) {
+        // Request Was Successful
+        if (response.ok) {
+            response.json().then(function (data) {
+
+                // Pass Response Data to DOM Function
+                displayIssues(data);
+                for (var i = 0; i < issues.length; i++) {
+
+                    // Create a Link Element to Take Users to The Issue On GitHub
+                    var issueEl = document.createElement("a");
+                    issueEl.classList = "list-item flex-row justify-space-between align-center";
+                    issueEl.setAttribute("href", issues[i].html_url);
+                    issueEl.setAttribute("target", "_blank");
+
+                    // Create Span to Hold Issue Title
+                    var titleEl = document.createElement("span");
+                    titleEl.textContent = issues[i].title;
+
+                    // Append to Container
+                    issueEl.appendChild(titleEl);
+
+                    // Create a Type Element
+                    var typeEl = document.createElement("span");
+
+                    // check if issue is an actual issue or a pull request
+                    if (issues[i].pull_request) {
+                        typeEl.textContent = "(Pull request)";
+                    } else {
+                        typeEl.textContent = "(Issue)";
+                    }
+
+                    // append to container
+                    issueEl.appendChild(typeEl);
+                }
+            });
+        } else {
+            alert("there was a problem with your request!");
+        }
+    });
 };
+var displayRepos = function (issues) {
+
+}
+
 
 getRepoIssues("facebook/react");
