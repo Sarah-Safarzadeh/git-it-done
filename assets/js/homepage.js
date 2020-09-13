c0be745f5f115c12ce5f0fd1518590e93e560add
spatial theory <68703408+Sarah-Safarzadeh@users.noreply.github.com>
2020-09-13 18:36:03 -0500

    fixed button issue and added three new buttons for javascript html css

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -80,5 +80,19 @@ var displayRepos = function(repos, searchTerm) {
   }
 };
 
+var getFeaturedRepos = function(language) {
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl).then(function(response) {
+    if (response.ok) {
+      response.json().then(function(data) {
+        displayRepos(data.items, language);
+      });
+    } else {
+      alert("Error: " + response.statusText);
+    }
+  });
+};
+
 // EVENT LISTENERS
 userFormEl.addEventListener("submit", formSubmitHandler);
